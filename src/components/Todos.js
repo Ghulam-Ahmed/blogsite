7f43c4a3f0e804132ad63173221093feeb026a4d
unknown <ghulamrock8@gmail.com>
2024-10-30 10:52:57 +0500

    react app

---
+++ src/components/Todos.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Todos() {
+  return (
+    <div>
+      <p>todo is okay</p>
+    </div>
+  );
+}
---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function footer() {
+  return (
+    <div>
+      <p>footer is also fine</p>
+    </div>
+  );
+}
